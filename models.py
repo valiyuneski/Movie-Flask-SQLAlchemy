2ab201becdde81f3fe045fe8a3367d433ab3ab94
Valentin Ionescu <valiyuneski@gmail.com>
2025-10-02 21:09:26 +0200

    Remove unique=True

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ db = SQLAlchemy()
 class User(db.Model):
     """Model representing a user."""
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False, unique=True)
+    name = db.Column(db.String(100), nullable=False)
 
     # One-to-many relationship: a user can have many movies
     movies = db.relationship('Movie', backref='user', lazy=True)
@@ -13,7 +13,7 @@ class User(db.Model):
 class Movie(db.Model):
     """Model representing a movie."""
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False, unique=True)
+    name = db.Column(db.String(100), nullable=False)
     director = db.Column(db.String(100), nullable=True)
     year = db.Column(db.Integer, nullable=False)
     poster_url = db.Column(db.String, nullable=False)
