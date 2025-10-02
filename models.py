bd5a8b47b8722776c5e3d4a5f3ec9958bf82887d
Valentin Ionescu <valiyuneski@gmail.com>
2025-10-02 10:06:09 +0200

    OMDb sometimes returns “N/A” — should be nullable=True

--- models.py
+++ models.py
@@ -11,7 +11,7 @@ class Movie(db.Model):
     """Model representing a movie."""
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    director = db.Column(db.String(100), nullable=False)
+    director = db.Column(db.String(100), nullable=True)
     year = db.Column(db.Integer, nullable=False)
     poster_url = db.Column(db.String, nullable=False)
 
