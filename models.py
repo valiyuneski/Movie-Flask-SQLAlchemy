33df910dbecaa9788993c04ef0658c55fa2ca563
Valentin Ionescu <valiyuneski@gmail.com>
2025-10-05 22:13:48 +0200

    parse_year() function correctly returns None for invalid years, but your Movie model still has year = db.Column(db.Integer, nullable=False). When parse_year() returns None, the database insertion will fail because the column doesn't accept NULL values.

--- models.py
+++ models.py
@@ -15,7 +15,7 @@ class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     director = db.Column(db.String(100), nullable=True)
-    year = db.Column(db.Integer, nullable=False)
+    year = db.Column(db.Integer, nullable=True)
     poster_url = db.Column(db.String, nullable=False)
 
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
